f946248f4a3f60bab922dab81e75d2c719284414
Claudiu Ionel <ionel.claudiu394@gmail.com>
2023-08-29 12:10:23 +0200

    added spacing and sized text in navbar

--- app/Components/Nav.js
+++ app/Components/Nav.js
@@ -2,15 +2,19 @@ import Link from "next/link";
 
 export default function nav() {
   return (
-    <nav>
-      <ul>
-        <li>
+    <nav className="min-w-screen flex justify-between">
+      <div className="px-5 py-2">
+        <h1 className="text-4xl">Julius Rene</h1>
+        <h3>Actor & Instrumentalist</h3>
+      </div>
+      <ul className="flex flex-row justify-end space-x-5 items-center">
+        <li className="flex align-middle">
           <Link href="/Gallery">Gallery</Link>
         </li>
-        <li>
+        <li className="flex align-middle">
           <Link href="/Showreel">Showreel</Link>
         </li>
-        <li>
+        <li className="flex align-middle">
           <Link href="/Contact">Contact</Link>
         </li>
       </ul>

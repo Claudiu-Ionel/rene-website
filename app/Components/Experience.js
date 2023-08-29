9502338954255b41fe4b1a64b50d6e43ef50097c
Ålexander Yurchenko <yurchenko@post.com>
2023-08-29 21:15:23 +0200

    Finished the desktop version of Experience

--- app/Components/Experience.js
+++ app/Components/Experience.js
@@ -2,15 +2,17 @@ import { experienceTimeline } from "../data/data";
 
 export default function Experience() {
   return (
-    <article>
-      <h2>Experience</h2>
-      <section>
+    <article className="w-full bg-white flex flex-col items-center">
+      <h2 className="font-primary text-3xl my-10">Experience</h2>
+      <section className="w-1/3">
         {experienceTimeline.map(({ year, projects }) => (
-          <div>
-            <p>{year}</p>
-            {/*  {projects.map(({ projectName }) => (
-              <p>{projectName}</p>
-            ))} */}
+          <div className="w-full flex mb-10">
+            <h3 className="text-2xl w-[20%]">{year}</h3>
+            <div className="w-[80%]">
+              {projects.map((project) => (
+                <p>{project}</p>
+              ))}
+            </div>
           </div>
         ))}
       </section>

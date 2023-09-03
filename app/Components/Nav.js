6ee5083c5f4f9146e5ac6a8ac64be60b2610caad
Claudiu Ionel <ionel.claudiu394@gmail.com>
2023-09-03 11:19:15 +0200

    added link to home page

--- app/Components/Nav.js
+++ app/Components/Nav.js
@@ -29,7 +29,9 @@ export default function nav({ setSidebarOpen, sidebarOpen }) {
         className={`hidden md:flex justify-between px-10 font-secondary relative z-10 top-0 `}
       >
         <div className="px-5 py-2 flex flex-col justify-center">
-          <h1 className="text-2xl font-primary">Julius Rene</h1>
+          <h1 className="text-2xl font-primary">
+            <Link href="/">Julius Rene</Link>
+          </h1>
         </div>
         <HamburgerMenu
           setSidebarOpen={setSidebarOpen}
@@ -60,7 +62,9 @@ export default function nav({ setSidebarOpen, sidebarOpen }) {
         className={`w-nav flex justify-between px-8 py-6 font-secondary absolute z-10 top-[40px] left-[40px]`}
       >
         <div className="text-backgroundColor px-5 py-2">
-          <h1 className="text-4xl font-primary">Julius Rene</h1>
+          <h1 className="text-4xl font-primary">
+            <Link href="/">Julius Rene</Link>
+          </h1>
           <h3 className="">Actor & Instrumentalist</h3>
         </div>
         <ul

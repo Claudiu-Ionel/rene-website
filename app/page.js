566285e92572b56947e0b91175114c33c531984d
Ålexander Yurchenko <yurchenko@post.com>
2023-09-09 16:10:52 +0200

    Removed Experience from main.js, replaced it with Welcome txt instead

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 "use client";
 import { useState } from "react";
-import Experience from "./Components/Experience";
 import Gallery from "./Components/Gallery";
 import ImageModal from "./Components/ImageModal";
 import { galleryImages } from "./data/data";
@@ -25,7 +24,22 @@ export default function Home() {
         limit={6}
         setModalImageGallery={setModalImageGallery}
       />
-      <Experience />
+      <div className="w-full bg-white p-5 flex justify-center font-secondary">
+        <div className="w-1/3">
+          <h2>This is a welcome text:</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+            eget auctor augue. Sed justo risus, tristique ut magna a,
+            ullamcorper consequat eros. Donec sodales ex vel leo molestie
+            consequat. Donec arcu tellus, vulputate porta tortor ac, porta
+            semper lacus. Pellentesque sed justo elementum, condimentum felis
+            vel, tempus mi. Fusce convallis tincidunt posuere. Integer dui urna,
+            tincidunt eu vulputate ac, luctus ac leo. Mauris laoreet ante vitae
+            imperdiet dignissim. Sed ultricies vestibulum ante. Aliquam est
+            neque, tempor mollis semper at, fringilla quis arcu.
+          </p>
+        </div>
+      </div>
     </>
   );
 }

b1608986d24aefdb7344b216321abd1ef11a921d
Claudiu Ionel <ionel.claudiu394@gmail.com>
2023-09-09 17:00:12 +0200

    changed gap between pictures in Gallery

--- app/Components/Gallery.js
+++ app/Components/Gallery.js
@@ -14,7 +14,7 @@ export default function Gallery({
   return (
     <section
       id="gallery-section"
-      className="grid grid-rows-2 grid-cols-3 bg-white w-full h-[90vw] justify-center gap-x-0 gap-y-10 py-16 px-14 md:px-10 md:gap-3 md:h-[80vw] sm:grid-rows-3 sm:grid-cols-2 sm:px-5 sm:h-[200vw] sm:gap-y-5"
+      className="grid grid-rows-2 grid-cols-3 bg-white w-full h-[95vw] justify-center gap-x-0 gap-y-3 py-16 px-14 md:px-10 md:gap-3 md:h-[80vw] sm:grid-rows-3 sm:grid-cols-2 sm:px-5 sm:h-[200vw] sm:gap-y-5"
     >
       {galleryImages.map((item, idx) => {
         return (
@@ -32,6 +32,7 @@ export default function Gallery({
                 alt={item.alt}
                 loading="lazy"
                 fill
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                 quality={100}
               />
             </div>

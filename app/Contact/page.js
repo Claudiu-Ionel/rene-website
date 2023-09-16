5d006677d66d440277eada72cfb369df2d202c2b
Claudiu Ionel <ionel.claudiu394@gmail.com>
2023-09-16 18:34:25 +0200

    import useState in contact page

--- app/Contact/page.js
+++ app/Contact/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "../layout";
 import { send } from "@emailjs/browser";
 

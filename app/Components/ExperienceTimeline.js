9ce2415a7ef3dc8e4193ffc3cf7f68d91c10eb87
Ålexander Yurchenko <yurchenko@post.com>
2023-09-23 21:17:22 +0200

    Created ExperienceTimeline component for Home

---
+++ app/Components/ExperienceTimeline.js
@@ -0,0 +1,22 @@
+import { experienceTimeline } from "../data/data";
+
+export default function ExperienceTimeline() {
+  return (
+    <article className="w-full bg-white flex flex-col items-center">
+      <h2 className="font-primary text-3xl my-10">Experience</h2>
+      <section className="w-1/3">
+        {experienceTimeline.map(({ year, projects }) => (
+          // eslint-disable-next-line react/jsx-key
+          <div className="w-full flex mb-10">
+            <h3 className="text-2xl w-[20%]">{year}</h3>
+            <div className="w-[80%]">
+              {projects.map((project) => (
+                <p key={project}>{project}</p>
+              ))}
+            </div>
+          </div>
+        ))}
+      </section>
+    </article>
+  );
+}

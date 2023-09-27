bdde1a47b322d8895a7f0b1d41b5910d3326989e
Ålexander Yurchenko <yurchenko@post.com>
2023-09-27 13:23:09 +0200

    Changed font size at ExperienceTimeline for mobile devices

--- app/Components/ExperienceTimeline.js
+++ app/Components/ExperienceTimeline.js
@@ -15,9 +15,9 @@ export default function ExperienceTimeline() {
       <section className="w-[60%] ipadAir:w-full">
         {experienceTimelineData.map(({ year, projects }) => (
           // eslint-disable-next-line react/jsx-key
-          <div className="w-full flex mb-10">
-            <h3 className="text-2xl w-[20%]">{year}</h3>
-            <div className="w-[80%]">
+          <div className="w-full flex mb-10 ">
+            <h3 className="text-2xl w-[20%] md:text-lg">{year}</h3>
+            <div className="w-[80%] md:text-base">
               {projects.map((project) => (
                 <p key={project}>{project}</p>
               ))}
